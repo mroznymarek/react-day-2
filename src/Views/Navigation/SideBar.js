257310b474c8fff80c1b0576b7cf180b658bb654
Marek Mroz <marekmroz@wp.pl>
2019-04-02 15:13:09 +0200

    sidebar

--- src/Views/Navigation/SideBar.js
+++ src/Views/Navigation/SideBar.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Drawer from '@material-ui/core/Drawer'
+
+const styles = {
+    drawer: {
+        width: 200
+    }
+}
 
 const SideBar = (props) => (
     <div>
+        <Drawer 
+            open={true}
+            onClose={() => console.log ('TOGGLE')}
+        >
+            <div style={styles.drawer}>
+            </div>
+        </Drawer>
         
     </div>
 )
