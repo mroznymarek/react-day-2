8b35b9beb527afda7b4aa2fd08acdcfaed37d4a6
Marek Mroz <marekmroz@wp.pl>
2019-04-02 10:10:01 +0200

    component in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Header from './Header';
 
 const App = () => {
-    return ( 
-        <h1>
-            HELLO WORLD
-        </h1>
-    )    
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
