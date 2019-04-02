7cf88dd2965af0eeeeb1c0922572dcb2876c6568
Marek Mroz <marekmroz@wp.pl>
2019-04-02 13:37:25 +0200

    display name component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Home from '.Views/Home'
-import Calculators from '.Views/Calculators'
+import Home from './Views/Home'
+import Calculators from './Views/Calculators'
+import DisplayName from './Views/DisplayName'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
 
       <Route exact={true} path={"/"} component={Home} />
       <Route path={"/calculators"} component={Calculators} />
+      <Route path={"/display-name/:name?"} component={DisplayName} />
     </Router>
   )
 }
